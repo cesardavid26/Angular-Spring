0288686eb504448b8defb5ba954c7400009dd921
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-03-09 20:13:00 -0500

    creando archivo clientes.json.ts

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
+import {CLIENTES} from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,20 +8,12 @@ import {Cliente} from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] =[
-
-    {id:1, nombre: 'César', apellido: 'Carvajal', email: 'cesar@gmail.com', createAt: '1997-26-10'},
-    {id:2, nombre: 'Andres', apellido: 'Gelves', email: 'andres@gmail.com', createAt: '1998-20-10'},
-    {id:3, nombre: 'Javier', apellido: 'Moncada', email: 'pacho@gmail.com', createAt: '1997-16-05'},
-    {id:4, nombre: 'Jarbir', apellido: 'Mejia', email: 'jarbir@gmail.com', createAt: '1998-16-12'},
-    {id:5, nombre: 'Brayan', apellido: 'Palomino', email: 'palo@gmail.com', createAt: '1996-28-10'},
-    {id:6, nombre: 'Camilo', apellido: 'Boada', email: 'boada@gmail.com', createAt: '1995-17-03'},
-    {id:7, nombre: 'Carlos', apellido: 'Pablos', email: 'pablos@gmail.com', createAt: '1997-06-11'}
-  ];
+  clientes: Cliente[] =[];
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] =[
+
+  {id:1, nombre: 'César', apellido: 'Carvajal', email: 'cesar@gmail.com', createAt: '1997-26-10'},
+  {id:2, nombre: 'Andres', apellido: 'Gelves', email: 'andres@gmail.com', createAt: '1998-20-10'},
+  {id:3, nombre: 'Javier', apellido: 'Moncada', email: 'pacho@gmail.com', createAt: '1997-16-05'},
+  {id:4, nombre: 'Jarbir', apellido: 'Mejia', email: 'jarbir@gmail.com', createAt: '1998-16-12'},
+  {id:5, nombre: 'Brayan', apellido: 'Palomino', email: 'palo@gmail.com', createAt: '1996-28-10'},
+  {id:6, nombre: 'Camilo', apellido: 'Boada', email: 'boada@gmail.com', createAt: '1995-17-03'},
+  {id:7, nombre: 'Carlos', apellido: 'Pablos', email: 'pablos@gmail.com', createAt: '1997-06-11'}
+];
