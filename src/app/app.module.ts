55ec9878d518b152b0a32dee25c84293168d1ab2
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-03-09 20:40:40 -0500

    creando la clase de Servicio CLienteService y la inyeccion de la dependecia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import {ClienteService} from './clientes/cliente.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +18,7 @@ import { ClientesComponent } from './clientes/clientes.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
