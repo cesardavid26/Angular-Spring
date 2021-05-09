7e3d4f219ab6bb663870bfc86110ac2199ce1eb4
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-05-09 13:04:32 -0500

    metodo delete en la clase service y componente de clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-clientes',
@@ -20,4 +21,37 @@ export class ClientesComponent implements OnInit {
   );
   }
 
+delete(cliente: Cliente): void{
+  const swalWithBootstrapButtons = swal.mixin({
+  customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger'
+  },
+  buttonsStyling: false
+})
+
+swalWithBootstrapButtons.fire({
+  title: '¿Está seguro?',
+  text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonText: '¡Sí, eliminar',
+  cancelButtonText: '¡No, cancelar!',
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+    this.clienteService.delete(cliente.id).subscribe(
+      response => {
+        this.clientes = this.clientes.filter(cli => cli !== cliente)
+        swalWithBootstrapButtons.fire(
+          '¡Cliente eliminado!',
+          `Cliente ${cliente.nombre} eliminado con éxito`,
+          'success'
+        )
+      }
+    )
+
+  }
+})
+}
 }
