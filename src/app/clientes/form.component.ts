a221e371824b38166dcdd6e6fd242bfaadb792d2
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-05-08 20:38:41 -0500

    instalacion de sweet alert

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente'
+import {ClienteService} from './cliente.service'
+import {Router} from '@angular/router'
+import swal from 'sweetalert2';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
-private cliente:Cliente = new Cliente()
-private titulo:string ="Crear cliente"
-  constructor() { }
+
+ cliente:Cliente = new Cliente()
+ titulo:string ="Crear cliente"
+
+  constructor(private clienteService: ClienteService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
   public create(): void{
-    console.log("Clicked!")
-    console.log(this.cliente)
+  this.clienteService.create(this.cliente)
+  .subscribe(cliente => {
+      this.router.navigate(['/clientes'])
+      swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con exito!`, 'success')
+      swal.update({icon: 'success'});
+  }
+
+  )
   }
 
 }
